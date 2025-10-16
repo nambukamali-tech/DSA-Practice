bb005787bcaa91031950df4ba1bdd9aa0648efb2
Nambu Kamali <nambukamali@gmail.com>
2025-10-16 22:05:34 +0530

    Add files via upload

---
+++ LoopReverseString.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reverseString
+{
+    internal class LoopReverseString
+    {
+        static void Main(string[] args)
+        {
+            string project = "Dotnet";
+            string reversed_project = "";
+            for(int i=project.Length-1;i>=0;i-- )//Loop method to Reverse the String
+            {
+                reversed_project += project[i];
+            }
+            Console.WriteLine("Original project is :" + project);
+            Console.WriteLine("Reversed project is :" + reversed_project);
+            
+        }
+
+    }
+}
