abea78aceffe0ec1253893663c116f7f57cadcc8
Nambu Kamali <nambukamali@gmail.com>
2025-10-28 21:46:55 +0530

    Add files via upload

---
+++ ReverseWordsdsa.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseWords
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the sentence");
+            string words = Console.ReadLine();
+            string resultWords = ReverseWords(words);
+            Console.WriteLine(resultWords);
+
+        }
+        static string ReverseWords(string str)
+        {
+            string[] RevWords = str.Split(' ');
+            Array.Reverse(RevWords);
+            string FinalWords = string.Join(" ", RevWords);
+            return FinalWords;
+        }
+    }
+}
