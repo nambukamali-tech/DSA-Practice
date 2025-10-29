2db17e7bfca1181179bbebe962235259270df823
Nambu Kamali <nambukamali@gmail.com>
2025-10-29 22:58:11 +0530

    Add files via upload

---
+++ countVowels.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace countVowels
+{
+    //Demo code of Counting the Number of vowels letters in given string
+    internal class countVowels
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Word :");
+            string word = Console.ReadLine();
+            int count = 0;
+            foreach(char c in word.ToLower())//Convert the String to Characters
+            {
+                if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' )//If any of the letters 'a e i o u' the count will updated
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine("The Number of Vowels in Word is" + count);
+        }
+    }
+}
